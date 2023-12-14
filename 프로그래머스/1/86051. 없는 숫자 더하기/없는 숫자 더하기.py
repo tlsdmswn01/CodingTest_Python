66c183a428a247d5974f2607989720dd39d2ac2d
Oooo_daon <135305102+tlsdmswn01@users.noreply.github.com>
2023-12-14 09:56:07 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,6 +1,14 @@
 def solution(numbers):
-    answer = 0
+    answer=0
     for i in range(10):
         if i not in numbers:
             answer+=i
+        else:
+            pass
+
+
+
+
+
+# ==============================
     return answer
