eb7fe1213a73cb21bcac8a3b48b40907f70d2aeb
Oooo_daon <135305102+tlsdmswn01@users.noreply.github.com>
2023-12-18 00:41:11 +0900

    [level unrated] Title: 배열의 길이를 2의 거듭제곱으로 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    n=1
+    while 2**n <len(arr):
+      n+=1
+
+    if len(arr)==n:
+      return (arr)
+    else:
+      while len(arr)!=2**n:
+        arr.append(0)
+    return arr
