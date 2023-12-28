4e076da13911dc2e9cf58368b6e2c43cec4913fb
Oooo_daon <135305102+tlsdmswn01@users.noreply.github.com>
2023-12-28 09:58:32 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,20 +1,16 @@
 def solution(nums):
     answer = 0
     l=len(nums)
-    b=[]
     for i in range(l):
         for k in range(i+1,l):
             for j in range(k+1,l):
-                a=nums[i]+nums[k]+nums[j]
-                b.append(a)
-    
-    for u in b:
-        p=True
-        
-        for h in range(2,u):
-            if u%h==0:
-                p=False
-        
-        if p:
-            answer+=1
+                a=True
+                b=nums[i]+nums[k]+nums[j]
+                for h in range(2,b):
+                    if b%h==0:
+                        a=False
+                        
+                if a:
+                    answer+=1
+                
     return answer
