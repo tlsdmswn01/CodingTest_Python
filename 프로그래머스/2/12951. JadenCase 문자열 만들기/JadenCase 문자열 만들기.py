0b9681390a800ff914030758a1110b02a54071ba
Oooo_daon <135305102+tlsdmswn01@users.noreply.github.com>
2024-01-02 16:33:58 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,11 @@
+def solution(s):
+    answer=[]
+    for i in s.split(' '):
+        i=list(i)
+        for k in range(len(i)):
+            if k==0:
+                i[k]=i[k].upper()
+            else:
+                i[k]=i[k].lower()
+        answer.append(''.join(i))
+    return ' '.join(answer)
