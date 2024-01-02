60e19b428ea9e611399b9028c12b56574dd1c500
Oooo_daon <135305102+tlsdmswn01@users.noreply.github.com>
2024-01-02 12:41:51 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 68.48 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,20 +1,13 @@
 def solution(n, m):
     answer = []  
-    for i in range(max(m,n),m*n+1):
-        if i%m==0 and i%n==0:
+    for i in range(min(n,m),0,-1):
+        if n%i==0 and m%i==0:
             answer.append(i)
             break
-        
-    if n>m:
-        while n%m!=0:
-                c=n%m
-                n,m=m,c
-        answer.append(m)
-    else:
-        while m%n!=0:
-            c=m%n
-            m,n=n,c
-        answer.append(n)    
+    for k in range(max(n,m),n*m+1):
+        if k%n==0 and k%m==0:
+            answer.append(k)
+            break
         
     answer.sort()
     return answer
