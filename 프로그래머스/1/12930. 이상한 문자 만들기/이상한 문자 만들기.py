7643eefca540398bc86dd804502b88836c96700b
Oooo_daon <135305102+tlsdmswn01@users.noreply.github.com>
2024-01-02 15:42:13 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,12 +1,14 @@
 def solution(s):
-    answer = []
+# 답안 작성 부분 ===============
+    answer=[]
     for i in s.split(' '):
         i=list(i)
-        print(i)
         for k in range(len(i)):
-            if k%2==0 or k==0:
+            if k==0 or k%2==0:
                 i[k]=i[k].upper()
             else:
                 i[k]=i[k].lower()
         answer.append(''.join(i))
+
+# ==============================
     return ' '.join(answer)
