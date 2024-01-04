b1afbeb676c76365b73303ba27e744e2d0f19d03
Oooo_daon <135305102+tlsdmswn01@users.noreply.github.com>
2024-01-05 02:25:07 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 2.09 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,14 +1,13 @@
 def solution(s):
     answer = []
-    a=''
-    count=0
-    num=0
+    a=0
+    c=0
     while s!='1':
-        count+=s.count('0')
+        a+=s.count('0')
         s=s.replace('0','')
         s=bin(len(s))[2:]
-        num+=1
-        
-    answer.append(num)
-    answer.append(count)
+        c+=1
+
+    answer.append(c)
+    answer.append(a)
     return answer
