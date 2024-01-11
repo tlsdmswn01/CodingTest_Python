0b8520062b44fdc870ce5e7b8a8276639344c7fe
Oooo_daon <135305102+tlsdmswn01@users.noreply.github.com>
2024-01-12 00:33:54 +0900

    [level 2] Title: 피보나치 수, Time: 95.11 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,7 +1,7 @@
 def solution(n):
-# 답안 작성 부분 ===============
-    a=0
-    b=1
+    answer = 0
+    pre =0
+    current=1
     for i in range(2,n+1):
-        a, b= a+b,a
-    return (a+b)%1234567
+        pre,current=current,pre+current
+    return current%1234567
